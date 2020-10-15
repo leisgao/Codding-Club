9b4521361d2c436a995c40e91121f5d9b09a754c
leisgao <50936873+leisgao@users.noreply.github.com>
2020-10-15 15:58:17 -0700

    Update LeetCodeProb35.java

--- LeetCodeProb35.java
+++ LeetCodeProb35.java
@@ -1,8 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int first = 0;
-        int last = nums.length()-1;
-        
-        return 0;
+        int first = 0
+        int last = nums.length;
+        while (first < last)
+        {
+            int mid = (first + last)/2;
+            if (nums[mid] >= target)
+            {
+                last = mid;
+            }
+            else
+            {
+                first = mid + 1;
+            }
+        }
+        return last;
     }
 }
